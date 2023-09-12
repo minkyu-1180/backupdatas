d90591bb34d6bdd1a60e3df30d867425e5c4b596
minkyu1180 <minkyu1180@naver.com>
2023-09-12 17:57:42 +0900

    algorithm 0912-2

--- backupdatas/백준스터디/7주차/0912/4. 24337 가희와 탑/bj24337.py
+++ backupdatas/백준스터디/7주차/0912/4. 24337 가희와 탑/bj24337.py
@@ -5,24 +5,43 @@ T = int(input())
 for tc in range(T):
     N, a, b = map(int, input().split())
 
-    if a + b - 1 >= N:
+    if a + b - 1 > N:
         print(-1)
     else:
-        max_v = max(a, b)
         result = [0] * N
+        # a = 1
+        if a == 1:
+            for i in range(b):
+                result[i] = b-i
+            for i in range(b, N):
+                result[i] = 1
+        # b = 1
+        elif b == 1:
+            idx = 1
+            for i in range(N-a, N):
+                result[i] = idx
+                idx += 1
+            for i in range(N-a):
+                result[i] = 1
 
-        result[N-b] = max_v
-        # print(result)
 
-        for i in range(1, b):
-            result[N-i] = i
-        # print(result)
+        else:
+            max_v = max(a, b)
 
-        for i in range(1, a):
-            result[N-b-(a-i)] = i
-        # print(result)
+            result[N-b] = max_v
+            # print(result)
 
-        for i in range(N-a-b+1):
-            result[i] = 1
-        print(result)
+            for i in range(1, b):
+                result[N-i] = i
+            # print(result)
 
+            for i in range(1, a):
+                result[N-b-(a-i)] = i
+            # print(result)
+
+            for i in range(N-a-b+1):
+                result[i] = 1
+
+        for i in range(N):
+            print(result[i], end = ' ')
+        print()
