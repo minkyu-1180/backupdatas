319cd0ae8cd4271c16138e44f3dfbcec19871038
minkyu1180 <minkyu1180@naver.com>
2023-09-14 20:58:47 +0900

    0914 algo study

--- backupdatas/백준스터디/7주차/0914/2. 2668 숫자고르기/bj2668.py
+++ backupdatas/백준스터디/7주차/0914/2. 2668 숫자고르기/bj2668.py
@@ -1,27 +1,43 @@
 # 백준 2668. 숫자고르기
 import sys
 sys.stdin = open("bj2668input.txt")
+
+
+input = sys.stdin.readline
+
 N = int(input())
-arr = [[0] * (N+1) for _ in range(2)]
-for j in range(1, N+1):
-    arr[0][j] = j
-    arr[1][j] = int(input())
-print(arr)
 
+# 1번 ~
+adj = [[] for _ in range(N + 1)]
 
+# i번과 이어진 값들
+for i in range(1, N + 1):
+    adj[i].append(int(input()))
 
+def dfs(num):
+    if visited[num] == 0:
+        visited[num] = 1
+        for i in adj[num]:
+            up.add(num)
+            bottom.add(i)
 
-start, next = arr[0][1], arr[1][1]
-visited = [[0] * (N+1) for _ in range(N+1)]
-visited[start][next] = 1
-stack = []
+            if up == bottom:
+                result.extend(list(bottom))
+                return
 
-while True:
-    start = next
-    next = arr[1][start]
-    # 돌고돌아 도착
-    if visited[start][next] == 1:
+            dfs(i)
+    visited[num] = 0
 
+result = []
+for i in range(1, N + 1):
+    visited = [0] * (N + 1)
+    up = set()
+    bottom = set()
+    dfs(i)
 
+result = list(set(result))
+result.sort()
 
-
+print(len(result))
+for i in range(len(result)):
+    print(result[i])
