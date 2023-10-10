626d8eb3db754607643445909b13b4b59d6ff021
minkyu-1180 <minkyu1180@naver.com>
2023-10-10 17:58:26 +0900

    baekjoon 1010

--- backupdatas/백준스터디/11주차/1010/3. 16236 아기 상어/bj16236.py
+++ backupdatas/백준스터디/11주차/1010/3. 16236 아기 상어/bj16236.py
@@ -47,16 +47,22 @@ for t in range(T):
         y, x = shark_pos
         min_dist = int(1e9)
         min_idx = -1
+        ny, nx = N, N
         for k in range(len(fishes)):
             i, j, fish_size = fishes[k]
             if shark_size <= fish_size:
                 continue
             if shark_size > fish_size:
                 # 더 큰 경우 -> 갈수 있는 루트가 있으면 비교
-                dist = abs(i - y) + abs(j - x)
-                if min_dist > dist and is_root(y, x, i, j, shark_size):
-                    min_dist = dist
-                    min_idx = k
+                if is_root(y, x, i, j, shark_size):
+                    dist = abs(i - y) + abs(j - x)
+                    if min_dist > dist:
+                        pass
+                    if i < ny or j < nx:
+                        min_dist = dist
+                        min_idx = k
+                        ny = i
+                        nx = j
         if min_idx == -1:
             break
 
